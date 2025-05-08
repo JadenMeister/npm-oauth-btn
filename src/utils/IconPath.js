cd5bfbc261ca402ce11f3923d8777bc1e62030d7
JadenMeister <bigdragon00@naver.com>
2025-05-08 19:10:04 +0900

    fix: svg 오류 수정

--- src/utils/IconPath.js
+++ src/utils/IconPath.js
@@ -1,20 +1,15 @@
 
-import naverRect from "../assets/icons/naverBtn/naver_rect.png"
-import naverCircle from "../assets/icons/naverBtn/naver_circle.png"
-import naverSquare from "../assets/icons/naverBtn/naver_square.png"
+import githubRect from "../assets/icons/githubBtn/github_rect.svg?url"
+import githubCircle from "../assets/icons/githubBtn/github_circle.svg?url"
+import githubSquare from "../assets/icons/githubBtn/github_square.svg?url"
 
-import googleRect from "../assets/icons/googleBtn/google_rect.svg"
-import googleCircle from "../assets/icons/googleBtn/google_circle.svg"
-import googleSquare from "../assets/icons/googleBtn/google_square.svg"
+import googleRect from "../assets/icons/googleBtn/google_rect.svg?url"
+import googleCircle from "../assets/icons/googleBtn/google_circle.svg?url"
+import googleSquare from "../assets/icons/googleBtn/google_square.svg?url"
 
-import kakaoRect from "../assets/icons/kakaoBtn/kakao_rect.svg"
-import kakaoCircle from "../assets/icons/kakaoBtn/kakao_circle.svg"
-import kakaoSquare from "../assets/icons/kakaoBtn/kakao_square.svg"
-
-
-import githubRect from "../assets/icons/githubBtn/github_rect.svg"
-import githubCircle from "../assets/icons/githubBtn/github_circle.svg"
-import githubSquare from "../assets/icons/githubBtn/github_square.svg"
+import kakaoRect from "../assets/icons/kakaoBtn/kakao_rect.svg?url"
+import kakaoCircle from "../assets/icons/kakaoBtn/kakao_circle.svg?url"
+import kakaoSquare from "../assets/icons/kakaoBtn/kakao_square.svg?url"
 
 export function IconPath(provider, shape="rect"){
     const key = `${provider}_${shape}`
